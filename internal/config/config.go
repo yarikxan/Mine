a6c51ae633735046d480d733a10a21743109bdd5
yarikxan <yarik.lemeshko@mail.ru>
2026-06-14 20:23:31 +0300

    init-commit

---
+++ internal/config/config.go
@@ -0,0 +1,36 @@
+package config
+
+import (
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	DBHost     string
+	DBPort     string
+	DBUser     string
+	DBPassword string
+	DBName     string
+	ServerPort string
+}
+
+func Load() *Config {
+	_ = godotenv.Load() // игнорируем ошибку, если .env нет
+
+	return &Config{
+		DBHost:     getEnv("DB_HOST", "localhost"),
+		DBPort:     getEnv("DB_PORT", "5432"),
+		DBUser:     getEnv("DB_USER", "postgres"),
+		DBPassword: getEnv("DB_PASSWORD", ""),
+		DBName:     getEnv("DB_NAME", "test"),
+		ServerPort: getEnv("SERVER_PORT", "8080"),
+	}
+}
+
+func getEnv(key, defaultValue string) string {
+	if value := os.Getenv(key); value != "" {
+		return value
+	}
+	return defaultValue
+}
